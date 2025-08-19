ae913251daeb9b04177e99755ab1873fbd7fd3b5
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-08-19 17:54:06 +0000

    mejora

--- clase 3/Estudiante.java
+++ clase 3/Estudiante.java
@@ -1,27 +1,28 @@
 import java.util.Arrays;
 
 public class Estudiante {
-    //atributos
+    // atributos
     private int id;
     private int cedula;
     private String nombre;
     private String facultad;
 
     // Constructor
-    public Estudiante(int id, int cedula, String nombre, String facultad){
+    public Estudiante(int id, int cedula, String nombre, String facultad) {
         this.id = id;
         this.cedula = cedula;
         this.nombre = nombre;
         this.facultad = facultad;
 
+    }
+
+    // metodo matricular cursos
+    public void matricularCursos(String[] cursos) {
+        System.out.println(Arrays.toString(cursos));
+    }
+
+    // metodo toString
+    public String toString() {
+        return "Estudiante: [id: " + id + " + Cedula: " + cedula + "Nombre: " + nombre + "Facultad: " + facultad + "]";
+    }
 }
-// metodo matricular cursos
-public void matricularCursos(String[]cursos){
-    System.out.println(Arrays.toString(cursos));
-}
-    //metodo toString
-    public String toString(){
-       return "Estudiante: [id: "+ id+ " + Cedula: " +cedula+ "Nombre: " + nombre + "Facultad: "+ facultad+"]";
-    }  
-}
-    
