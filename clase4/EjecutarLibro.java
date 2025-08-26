36c373b0968d56eb51470327a7549960657dfab0
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-08-26 17:35:31 +0000

    d

---
+++ clase4/EjecutarLibro.java
@@ -0,0 +1,17 @@
+
+package clase4;
+
+public class EjecutarLibro{
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Libro
+    Libro [] libros = new Libro[5];
+
+    libros[0] = new Libro("El Quijote", "Miguel de Cervantes", 16.50);
+    libros[1] = new Libro("Cien Años de Soledad", "Gabriel García Márquez", 19.600);
+    libros[2] = new Libro("1984", "George Orwell", 19.400);
+    libros[3] = new Libro("La Sombra del Viento", "Carlos Ruiz Zafón", 20.000);
+    libros[4] = new Libro("El Principito", "Antoine de Saint-Exupéry", 21.300);
+    }
+}
+
+
---
+++ clase4/Libro.java
@@ -0,0 +1,36 @@
+public class Libro {
+    
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    public Libro(String titulo, String autor, double precio){
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }  
+    
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+     @Override
+        public  toString (){
+     return "Libro:[titulo=" + ", autor=" + autor + ", precio=" + precio +"]";
+        }
+     
+        //calcular precio total de los libros en el arreglo
+    public static double calcularPrecioTol(Libro[]libros){
+        double total =0.0;
+         for(int i=0; i< libros.length; i++){
+            total += libros[i].getPrecio();
+         }
+          return total;
+    }
+}
