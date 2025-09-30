7b64b243b5a6d4765395b0641edb131d0ce3ee55
torresangulovalentina-bit <torresangulovalentina@gmail.com>
2025-09-30 16:35:31 +0000

    h

--- TallerPilasColas/TallerPilasColas/panSimilar.java
+++ TallerPilasColas/TallerPilasColas/panSimilar.java
@@ -8,16 +8,16 @@ import java.util.List;
  // Pan similar
 public class panSimilar {
     
-    public static boolean panSimilar(List<String> s1, List<String> s2) {
+    public static boolean panSimilar(List<String> sandwi1, List<String> sandwi2) {
         Deque<String> d1 = new ArrayDeque<>();
         Deque<String> d2 = new ArrayDeque<>();
 
         // encolar los elementos
-        for (int i = 0; i < s1.size(); i++) {
-            d1.addLast(s1.get(i));
+        for (int i = 0; i < sandwi1.size(); i++) {
+            d1.addLast(sandwi1.get(i));
         }
-        for (int i = 0; i < s2.size(); i++) {
-            d2.addLast(s2.get(i));
+        for (int i = 0; i < sandwi2.size(); i++) {
+            d2.addLast(sandwi2.get(i));
         }
 
         if (d1.isEmpty() || d2.isEmpty()) {
